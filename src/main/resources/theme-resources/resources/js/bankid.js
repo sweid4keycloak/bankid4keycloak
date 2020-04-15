d573708a9357ccb79c1a843343816813d7d76dde
Pontus Ullgren <ullgren@gmail.com>
2020-04-15 09:47:39 +0200

    Fix för att hämta state när vi kör via POST

--- src/main/resources/theme-resources/resources/js/bankid.js
+++ src/main/resources/theme-resources/resources/js/bankid.js
@@ -21,24 +21,18 @@ function poll(fn, callback, errback, timeout, interval) {
 }
 
 function redirectToDone() {
-	window.location.href = "done?state=" + getParameterByName('state');
+	window.location.href = "done?state=" + getStateValue();
 }
 
 
 function redirectToError(errorCode) {
-	window.location.href = "error?state=" +  getParameterByName('state') + "&code=" + errorCode;
+	window.location.href = "error?state=" +  getStateValue() + "&code=" + errorCode;
 }
 
 function redirectToCancel(errorCode) {
-	window.location.href = "cancel?state=" +  getParameterByName('state');
+	window.location.href = "cancel?state=" +  getStateValue();
 }
 
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+function getStateValue() {
+	 return document.getElementById("form_state").getAttribute("value");
 }
