c549335b78b4eb4870b071bd58fae5775e28b2d1
Pontus Ullgren <pontus.ullgren@redpill-linpro.com>
2023-06-27 21:08:22 +0200

    Updated to keycloak main branch version 999.0.0-SNAPSHOT

--- src/main/java/org/keycloak/broker/bankid/BankidEndpoint.java
+++ src/main/java/org/keycloak/broker/bankid/BankidEndpoint.java
@@ -12,17 +12,17 @@ import java.util.concurrent.TimeUnit;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.Response.Status;
-import javax.ws.rs.core.CacheControl;
+import jakarta.ws.rs.FormParam;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.ResponseBuilder;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.core.CacheControl;
 
 import org.infinispan.Cache;
 import org.jboss.logging.Logger;
