62191958d08d1626b20593a51fb04bbb76358cea
Harikant Kumar <harikant.kumar@lftt.se>
2023-08-15 11:24:48 +0200

    adding Custom Authentication Flows for SPAR call

---
+++ src/main/java/org/keycloak/lf/sparauthenticator/SparAuthenticatorFactory.java
@@ -0,0 +1,94 @@
+package org.keycloak.lf.sparauthenticator;
+
+
+import static org.keycloak.provider.ProviderConfigProperty.STRING_TYPE;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+public class SparAuthenticatorFactory implements AuthenticatorFactory {
+	 public static final String ID = "ipauthenticator";
+
+	    private static final Authenticator AUTHENTICATOR_INSTANCE = new SPARAuthenticator();
+	    static final String USER_SERVICE_URL = "user_service_url";
+
+	    @Override
+	    public Authenticator create(KeycloakSession keycloakSession) {
+	        return AUTHENTICATOR_INSTANCE;
+	    }
+
+	    @Override
+	    public String getDisplayType() {
+	        return "SPAR Authenticator";
+	    }
+
+	    @Override
+	    public boolean isConfigurable() {
+	        return true;
+	    }
+
+	    @Override
+	    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+	        return new AuthenticationExecutionModel.Requirement[] { AuthenticationExecutionModel.Requirement.REQUIRED };
+	    }
+
+	    @Override
+	    public boolean isUserSetupAllowed() {
+	        return false;
+	    }
+
+	    @Override
+	    public String getHelpText() {
+	        return "Call User Serivce to get the user dertails from SPAR";
+	    }
+
+	    @Override
+	    public List<ProviderConfigProperty> getConfigProperties() {
+	        ProviderConfigProperty userServiceSparCallEndPoint = new ProviderConfigProperty();
+
+	        userServiceSparCallEndPoint.setType(STRING_TYPE);
+	        userServiceSparCallEndPoint.setName(USER_SERVICE_URL);
+	        userServiceSparCallEndPoint.setLabel("Url of LF User Service");
+	        userServiceSparCallEndPoint.setHelpText("API endpoint that will pull the user information from SPAR");
+	        
+	        ProviderConfigProperty apiKey = new ProviderConfigProperty();
+
+	        apiKey.setType(STRING_TYPE);
+	        apiKey.setName(USER_SERVICE_URL);
+	        apiKey.setLabel("API Key of User Service");
+	        apiKey.setHelpText("User Service API endpoint API Key, that will pull the user information from SPAR");
+	        List<ProviderConfigProperty> conficCollection = Collections.singletonList(userServiceSparCallEndPoint);
+	        conficCollection.add(apiKey);
+	        return conficCollection;
+	    }
+
+	    @Override
+	    public String getReferenceCategory() {
+	        return null;
+	    }
+
+	    @Override
+	    public void init(Config.Scope scope) {
+	    }
+
+	    @Override
+	    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+	    }
+
+	    @Override
+	    public void close() {
+	    }
+
+	    @Override
+	    public String getId() {
+	        return ID;
+	    }
+}
---
+++ src/main/resources/theme-resources/templates/error.ftl
@@ -0,0 +1,2 @@
+User Service call error
+TODO: Create a proper html for error page
